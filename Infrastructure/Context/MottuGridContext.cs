964f2495498b2f46757a6016dcd84bd443eb7e9a
Pedro Henrique <ph6704938@gmail.com>
2025-05-09 11:25:24 -0300

    feat: add MottuGridContext class for database connection

---
+++ Infrastructure/Context/MottuGridContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MottuGrid_Dotnet.Infrastructure.Context
+{
+    public class MottuGridContext(DbContextOptions<MottuGridContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+        }
+    }
+}
