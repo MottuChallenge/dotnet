ec94fc30ec4bfbc42f4035d124c783949565aea1
Pedro Henrique <ph6704938@gmail.com>
2025-05-12 11:38:58 -0300

    feat: add V_3 migration and update model snapshot

---
+++ Migrations/20250512143414_V_3.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MottuGrid_Dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class V_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EngineType",
+                table: "Motorcycles",
+                type: "NVARCHAR2(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "NUMBER(10)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EngineType",
+                table: "Motorcycles",
+                type: "NUMBER(10)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
