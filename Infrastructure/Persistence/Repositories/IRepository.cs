48ddea3ac888848b061feb2089a7ae6fccee440a
Pedro Henrique <ph6704938@gmail.com>
2025-05-12 23:16:08 -0300

    feat: Add repository pattern implementation and restructure project

---
+++ Infrastructure/Persistence/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MottuGrid_Dotnet.Infrastructure.Persistence.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        void UpdateAsync(T entity);
+        void DeleteAsync(int id);
+    }
+}
