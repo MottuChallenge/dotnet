5583fbd914f33336d1fc94338a62cec71f723561
Pedro Henrique <ph6704938@gmail.com>
2025-05-13 21:38:10 -0300

    feat: enhance BranchResponse constructor to support full initialization

--- Domain/DTO/Response/BranchResponse.cs
+++ Domain/DTO/Response/BranchResponse.cs
@@ -10,10 +10,15 @@
         public AddressResponse Address { get; set; }
         public List<YardResponse> Yards { get; set; }
 
-        public BranchResponse(Guid id, AddressResponse address)
+        public BranchResponse(Guid id, string name, string CNPJ, string phone, string email, AddressResponse address, List<YardResponse> yards)
         {
             this.Id = id;
+            this.Name = name;
+            this.CNPJ = CNPJ;
+            this.Phone = phone;
+            this.Email = email;
             this.Address = address;
+            this.Yards = yards;
         }
     }
 }
