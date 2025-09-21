3e0173608f11447f18de54a1a2c1ea6f15e0d1f4
Pedro Henrique <ph6704938@gmail.com>
2025-09-21 17:03:47 -0300

    feat: add GetYardEntityByIdUseCase for yard retrieval

---
+++ MottuChallenge.Application/UseCases/Yards/GetYardEntityByIdUseCase.cs
@@ -0,0 +1,27 @@
+﻿using MottuChallenge.Application.UseCases.Addresses;
+using MottuChallenge.Domain.Entities;
+using MottuChallenge.Infrastructure.Repositories;
+
+namespace MottuChallenge.Application.UseCases.Yards
+{
+    public class GetYardEntityByIdUseCase
+    {
+        private readonly IYardRepository _yardRepository;
+        private readonly FindAddressByIdUseCase _findAddressByIdUseCase;
+
+        public GetYardEntityByIdUseCase(IYardRepository yardRepository, FindAddressByIdUseCase findAddressByIdUseCase)
+        {
+            _yardRepository = yardRepository;
+            _findAddressByIdUseCase = findAddressByIdUseCase;
+        }
+
+        public async Task<Yard> FindYardById(Guid id)
+        {
+            var yard = await _yardRepository.GetYardByIdAsync(id);
+            var address = await _findAddressByIdUseCase.GetAddressByIdAsync(yard.AddressId);
+            yard.Address = address;
+
+            return yard;
+        }
+    }
+}
