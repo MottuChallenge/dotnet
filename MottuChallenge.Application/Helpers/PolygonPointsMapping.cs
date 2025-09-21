4fcb300b9d961a008083417fbad3845ee7447e3d
Pedro Henrique <ph6704938@gmail.com>
2025-09-21 19:47:07 -0300

    refactor: CreateListOfPointResponseDto method

--- MottuChallenge.Application/Helpers/PolygonPointsMapping.cs
+++ MottuChallenge.Application/Helpers/PolygonPointsMapping.cs
@@ -1,15 +1,16 @@
 ﻿using MottuChallenge.Application.DTOs.Response;
 using MottuChallenge.Domain.Entities;
+using MottuChallenge.Domain.ValueObjects;
 
 namespace MottuChallenge.Application.Helpers
 {
     internal static class PolygonPointsMapping
     {
-        public static List<PointResponseDto> CreateListOfPointResponseDto(Yard yard)
+        public static List<PointResponseDto> CreateListOfPointResponseDto(IEnumerable<PolygonPoint> polygonPoint)
         {
             var points = new List<PointResponseDto>();
 
-            foreach (var point in yard.Points)
+            foreach (var point in polygonPoint)
             {
                 points.Add(new PointResponseDto { PointOrder = point.PointOrder, X = point.X, Y = point.Y });
             }
