8f14038fb6df09ed8e3ea12b7e6cd5264db5a608
Pedro Henrique <ph6704938@gmail.com>
2025-09-21 17:04:42 -0300

    feat: add Dependency Injection for use case services

---
+++ MottuChallenge.Application/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MottuChallenge.Application.UseCases.Addresses;
+using MottuChallenge.Application.UseCases.Sectors;
+using MottuChallenge.Application.UseCases.SectorTypes;
+using MottuChallenge.Application.UseCases.Spots;
+using MottuChallenge.Application.UseCases.Yards;
+
+namespace MottuChallenge.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddUseCases(this IServiceCollection services)
+        {
+            services.AddScoped<FindAddressByCepUseCase>();
+            services.AddScoped<FindAddressByIdUseCase>();
+            services.AddScoped<CreateSectorUseCase>();
+            services.AddScoped<GetAllSectorsUseCase>();
+            services.AddScoped<GetSectorByIdUseCase>();
+            services.AddScoped<CreateSectorTypeUseCase>();
+            services.AddScoped<DeleteSectorTypeUseCase>();
+            services.AddScoped<GetAllSectorTypesUseCase>();
+            services.AddScoped<UpdateSectorTypeUseCase>();
+            services.AddScoped<GenerateSpotsUseCase>();
+            services.AddScoped<CreateYardUseCase>();
+            services.AddScoped<GetAllYardsUseCase>();
+            services.AddScoped<GetYardByIdUseCase>();
+            services.AddScoped<GetYardEntityByIdUseCase>();
+
+            return services;
+        }
+    }
+}
