7d09319bcc34b7bf10d81db1309ced049309a5c5
Pedro Henrique <ph6704938@gmail.com>
2025-09-21 18:07:29 -0300

    feat: add SectorCreateDtoValidator for input validation

---
+++ MottuChallenge.Application/DTOs/Validations/SectorCreateDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MottuChallenge.Application.DTOs.Request;
+
+namespace MottuChallenge.Application.DTOs.Validations
+{
+    public class SectorCreateDtoValidator : AbstractValidator<SectorCreateDto>
+    {
+        public SectorCreateDtoValidator()
+        {
+            RuleFor(x => x.YardId)
+                .NotEmpty().WithMessage("O YardId é obrigatório.");
+
+            RuleFor(x => x.SectorTypeId)
+                .NotEmpty().WithMessage("O SectorTypeId é obrigatório.");
+
+            RuleFor(x => x.Points)
+                .NotEmpty().WithMessage("O setor deve ter pelo menos um ponto definido.")
+                .Must(points => points.Count >= 3)
+                .WithMessage("O setor deve ter no mínimo 3 pontos.");
+
+            RuleForEach(x => x.Points).SetValidator(new CreatePolygonPointDtoValidator());
+        }
+    }
+}
