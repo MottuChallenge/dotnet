788096dc9c3890a362253763375135dc42c2ac9c
Pedro Henrique <ph6704938@gmail.com>
2025-09-21 15:36:00 -0300

    fix: remove AddressService and add use cases to follow DDD

---
+++ MottuChallenge.Application/UseCase/Addresses/FindAddressByCepUseCase.cs
@@ -0,0 +1,22 @@
+﻿using MottuChallenge.Application.Services;
+using MottuChallenge.Domain.Entities;
+
+
+namespace MottuChallenge.Application.UseCase.Addresses
+{
+    public class FindAddressByCepUseCase
+    {
+        private readonly IAddressProvider _addressProvider;
+
+        public FindAddressByCepUseCase(IAddressProvider addressProvider)
+        {
+            _addressProvider = addressProvider;
+        }
+
+        public async Task<Address> FindAddressByCep(string cep, string number)
+        {
+            var address = await _addressProvider.GetAddressByCepAsync(cep, number);
+            return address;
+        }
+    }
+}
---
+++ MottuChallenge.Application/UseCase/Addresses/FindAddressByIdUseCase.cs
@@ -0,0 +1,15 @@
+﻿using MottuChallenge.Domain.Entities;
+using MottuChallenge.Infrastructure.Repositories;
+
+namespace MottuChallenge.Application.UseCase.Addresses
+{
+    public class FindAddressByIdUseCase(IAddressRepository addressRepository)
+    {
+        private readonly IAddressRepository _addressRepository = addressRepository;
+
+        public async Task<Address> GetAddressByIdAsync(Guid id)
+        {
+            return await _addressRepository.GetAddressByIdAsync(id);
+        }
+    }
+}
