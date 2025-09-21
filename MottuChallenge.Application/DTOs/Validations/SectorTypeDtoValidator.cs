f02bc7401a9e5d624158b70cafd0fc31f500c881
Pedro Henrique <ph6704938@gmail.com>
2025-09-21 18:07:13 -0300

    feat: add SectorTypeDtoValidator for input validation

---
+++ MottuChallenge.Application/DTOs/Validations/SectorTypeDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MottuChallenge.Application.DTOs.Request;
+
+namespace MottuChallenge.Application.DTOs.Validations
+{
+    public class SectorTypeDtoValidator : AbstractValidator<SectorTypeDto>
+    {
+        public SectorTypeDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("O nome do tipo de setor é obrigatório.")
+                .MaximumLength(50).WithMessage("O nome do tipo de setor deve ter no máximo 50 caracteres.");
+        }
+    }
+}
