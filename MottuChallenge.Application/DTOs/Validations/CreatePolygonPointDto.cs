b57244c648f5d143612904e8a1ac1b3155c18a9f
Pedro Henrique <ph6704938@gmail.com>
2025-09-21 18:06:00 -0300

    feat: add validation for CreatePolygonPointDto properties

---
+++ MottuChallenge.Application/DTOs/Validations/CreatePolygonPointDto.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MottuChallenge.Application.DTOs.Request;
+
+namespace MottuChallenge.Application.DTOs.Validations
+{
+    public class CreatePolygonPointDtoValidator : AbstractValidator<CreatePolygonPointDto>
+    {
+        public CreatePolygonPointDtoValidator()
+        {
+            RuleFor(x => x.PointOrder)
+                .GreaterThan(0).WithMessage("A ordem do ponto deve ser maior que 0.");
+
+            RuleFor(x => x.X)
+                .NotNull().WithMessage("O valor X do ponto é obrigatório.");
+
+            RuleFor(x => x.Y)
+                .NotNull().WithMessage("O valor Y do ponto é obrigatório.");
+        }
+    }
+}
