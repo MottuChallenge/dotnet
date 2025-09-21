5842884f1555d4166503394419fc5006fbd61124
Pedro Henrique <ph6704938@gmail.com>
2025-09-21 19:34:41 -0300

    feat: add GetSectorTypeByIdUseCase for sector retrieval

---
+++ MottuChallenge.Application/UseCases/SectorTypes/GetSectorTypeByIdUseCase.cs
@@ -0,0 +1,22 @@
+﻿using MottuChallenge.Application.Repositories;
+using MottuChallenge.Domain.Entities;
+
+
+namespace MottuChallenge.Application.UseCases.SectorTypes
+{
+    public class GetSectorTypeByIdUseCase
+    {
+        private readonly ISectorTypeRepository _sectorTypeRepository;
+        public GetSectorTypeByIdUseCase(ISectorTypeRepository sectorTypeRepository)
+        {
+            _sectorTypeRepository = sectorTypeRepository;
+        }
+        public async Task<SectorType> FindSectorTypeById(Guid id)
+        {
+            var sectorType = await _sectorTypeRepository.FindByIdAsync(id);
+            if (sectorType == null)
+                throw new KeyNotFoundException("SectorType not found.");
+            return sectorType;
+        }
+    }
+}
