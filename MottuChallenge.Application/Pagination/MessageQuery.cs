088e74a3263598fec84571d6f59ce5a1c2709890
Pedro Henrique <ph6704938@gmail.com>
2025-09-22 10:35:20 -0300

    feat: add pagination utilities to support paginated queries

---
+++ MottuChallenge.Application/Pagination/MessageQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace MottuChallenge.Application.Pagination
+{
+    public class MessageQuery
+    {
+        public string? Plate { get; init; }
+       
+    }
+}
---
+++ MottuChallenge.Application/Pagination/PaginatedResult.cs
@@ -0,0 +1,18 @@
+﻿namespace MottuChallenge.Application.Pagination
+{
+    public sealed class PaginatedResult<T>
+    {
+        public IReadOnlyList<T> Items { get; }
+        public int TotalItems { get; }
+        public int Page { get; }
+        public int PageSize { get; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalItems / PageSize);
+
+        public bool HasPrevious => Page > 1;
+
+        public bool HasNext => Page < TotalPages;
+
+        public PaginatedResult(IReadOnlyList<T> items, int totalItems, int page, int pageSize)
+            => (Items, TotalItems, Page, PageSize) = (items, totalItems, page, pageSize);
+    }
+}
