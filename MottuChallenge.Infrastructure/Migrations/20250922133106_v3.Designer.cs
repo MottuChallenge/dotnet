af9ceb90a17f0e277312a6c671886bf59edb3a5a
Pedro Henrique <ph6704938@gmail.com>
2025-09-22 10:34:16 -0300

    refactor: Make `SpotId` nullable

--- MottuChallenge.Infrastructure/Migrations/20250922133106_v3.Designer.cs
+++ MottuChallenge.Infrastructure/Migrations/20250922133106_v3.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MottuChallenge.Infrastructure.Persistence;
 
@@ -10,9 +11,11 @@ using MottuChallenge.Infrastructure.Persistence;
 namespace MottuChallenge.Infrastructure.Migrations
 {
     [DbContext(typeof(MottuChallengeContext))]
-    partial class MottuChallengeContextModelSnapshot : ModelSnapshot
+    [Migration("20250922133106_v3")]
+    partial class v3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MottuChallenge.Infrastructure/Migrations/20250922133106_v3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MottuChallenge.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class v3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SpotId",
+                table: "Motorcycles",
+                type: "char(36)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "char(36)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SpotId",
+                table: "Motorcycles",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "char(36)",
+                oldNullable: true);
+        }
+    }
+}
