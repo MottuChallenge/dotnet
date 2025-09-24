c32a1ab837bfdd8d5cbd13159ea96d0c959e5de5
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-24 09:48:27 -0300

    feat: add validation to validate if motorcycle exist inside Spot

---
+++ MottuChallenge.Application/Helpers/ValidateIfExistMotorcycleInsideSpot.cs
@@ -0,0 +1,33 @@
+﻿using MottuChallenge.Domain.Entities;
+
+namespace MottuChallenge.Application.Helpers
+{
+    internal static class ValidateIfExistMotorcycleInsideSpot
+    {
+        public static void ValidateMotorcycleInsideSpot(Sector sector, Guid spotId)
+        {
+            if (sector == null)
+            {
+                throw new ArgumentNullException(nameof(sector), "Sector cannot be null.");
+            }
+
+            // Verifique se a coleção de Spots não é nula ou vazia
+            if (sector.Spots == null || !sector.Spots.Any())
+            {
+                throw new InvalidOperationException("The sector does not contain any spots.");
+            }
+
+            var spot = sector.Spots.FirstOrDefault(s => s.SpotId == spotId);
+            if (spot == null)
+            {
+                throw new KeyNotFoundException("Spot not found");
+            }
+
+
+            if (spot.Status == Domain.Enums.SpotStatus.OCCUPIED)
+            {
+                throw new ArgumentException("This Spot is already occupied");
+            }
+        }
+    }
+}
