c00456ffa0d18954e551661756ad3dd628d98198
Pedro Henrique <ph6704938@gmail.com>
2025-10-27 09:23:59 -0300

    feat: add JWT authentication setup and employee repository registration

--- MottuChallenge.Infrastructure/DependencyInjection.cs
+++ MottuChallenge.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using MottuChallenge.Application.Configurations;
 using MottuChallenge.Application.Interfaces;
 using MottuChallenge.Application.Repositories;
 using MottuChallenge.Infrastructure.Persistence;
 using MottuChallenge.Infrastructure.Repositories;
+using MottuChallenge.Infrastructure.Security;
 using MottuChallenge.Infrastructure.Services;
 
 namespace MottuChallenge.Infrastructure
@@ -32,6 +35,7 @@ namespace MottuChallenge.Infrastructure
             services.AddScoped<ISectorRepository, SectorRepository>();
             services.AddScoped<ISectorTypeRepository, SectorTypeRepository>();
             services.AddScoped<IMotorcycleRepository, MotorcycleRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             return services;
         }
 
@@ -40,10 +44,31 @@ namespace MottuChallenge.Infrastructure
             services.AddHttpClient<IAddressProvider, FindAddressByApiViaCep>();
             return services;
         }
+
+        private static IServiceCollection AddSecurity(this IServiceCollection services, JwtSettings jwtSettings)
+        {
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {
+                    var secret = jwtSettings.SecretKey;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidIssuer = jwtSettings.Issuer,
+                        ValidAudience = jwtSettings.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret))
+                    };
+                });
+            services.AddScoped<JwtTokenService>();
+            return services;
+        }
+        
         
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, Settings settings)
         {
             services.AddDbContext(settings.ConnectionStrings);
+            services.AddSecurity(settings.Jwt);
             services.AddRepositories();
             services.AddServices();
             return services;
